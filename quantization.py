739450596ab20f33ae32e5d98bbdb9c215307e7d
fed12345 <f.magri12@gmail.com>
2023-10-19 12:05:29 +0200

    new nets

--- quantization.py
+++ quantization.py
@@ -2,8 +2,8 @@ from onnxruntime.quantization import quantize_dynamic
 from onnxruntime.quantization import QuantizationMode
 
 # Load the model
-onnx_model_path = 'crazygcnet.onnx'
-quantized_model_path = 'crazygcnet_quantized.onnx'
+onnx_model_path = 'results/onnx/crazygcnet_0_gates.onnx'
+quantized_model_path = 'results/quantized/crazygcnet_0_gates_quantized.onnx'
 
 # Quantize
 quantized_model = quantize_dynamic(onnx_model_path, quantized_model_path)
